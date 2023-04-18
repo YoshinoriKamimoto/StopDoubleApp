ae2c59a224e62f7392e903040e9e092c56260fd1
YoshinoriKamimoto <k.yoshinori0210@gmail.com>
2023-04-18 13:16:32 +0900

    2重起動防止サンプルフォーム

---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // 2重起動防止
+            // Mutex名を決める（必ずアプリケーション固有の文字列に変更すること！）
+            string mutexName = "二重起動防止検証ツール";
+            // Mutexオブジェクトを作成する
+            System.Threading.Mutex mutex = new System.Threading.Mutex(false, mutexName);
+
+            bool hasHandle = false;
+            try
+            {
+                try
+                {
+                    // ミューテックスの所有権を要求する
+                    hasHandle = mutex.WaitOne(0, false);
+                }
+                catch (System.Threading.AbandonedMutexException)
+                {
+                    // 別のアプリケーションがミューテックスを解放しないで終了した時
+                    hasHandle = true;
+                }
+                //ミューテックスを得られたか調べる
+                if (hasHandle == false)
+                {
+                    // 得られなかった場合は、すでに起動していると判断して終了
+                    MessageBox.Show("すでにアプリケーションが起動中です。",
+                                    "エラー",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                    return;
+                }
+
+                //はじめからMainメソッドにあったコードを実行
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            finally
+            {
+                if (hasHandle)
+                {
+                    //ミューテックスを解放する
+                    mutex.ReleaseMutex();
+                }
+                mutex.Close();
+            }
+        }
+    }
+}
